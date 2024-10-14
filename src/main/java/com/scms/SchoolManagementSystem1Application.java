ca6e3ad89aef1377617d47373380058932405994
Arunabh <arunabhsharma54321@gmail.com>
2024-10-14 23:19:37 +0530

    commit

--- src/main/java/com/scms/SchoolManagementSystem1Application.java
+++ src/main/java/com/scms/SchoolManagementSystem1Application.java
@@ -3,10 +3,18 @@ package com.scms;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
 @SpringBootApplication
 public class SchoolManagementSystem1Application {
 
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.load();
+        System.setProperty("DB_HOST", dotenv.get("DB_HOST"));
+        System.setProperty("DB_PORT", dotenv.get("DB_PORT"));
+        System.setProperty("DB_NAME", dotenv.get("DB_NAME"));
+        System.setProperty("DB_USERNAME", dotenv.get("DB_USERNAME"));
+        System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
 		SpringApplication.run(SchoolManagementSystem1Application.class, args);
 	}
 
